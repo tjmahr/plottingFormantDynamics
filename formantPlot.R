c0810543a4e54c8b71da08451622437d2c78f8b1
Tristan Jay Mahr <Mahr@Wisc.Edu>
2017-02-16 10:21:36 -0600

    spaghetti plot

--- formantPlot.R
+++ formantPlot.R
@@ -5,33 +5,66 @@ library(reshape2)
 # read in data
 formantData <- read.csv("formantMeasures.csv")
 
-# plot formants for head
-# get F1 measures
-ehF1 <- formantData[formantData$vowel == "head", 1:7]
-colnames(ehF1) <- c("sound","vowel","20", "35", "50","65", "80")
-ehF1 <- melt(ehF1, id=c("sound","vowel"))
+# add token column so individual formant trajectories can be identified
+formantData$token <- seq_len(nrow(formantData))
 
-# get F2 measures
-ehF2 <- formantData[formantData$vowel == "head", c(1:2,8:12)]
-colnames(ehF2) <- c("sound","vowel","20", "35", "50","65", "80")
-ehF2 <- melt(ehF2, id=c("sound","vowel"))
+# get F1 measures in long format
+ehF1 <- formantData[c(1:7, 18)]
+colnames(ehF1) <- c("sound", "vowel", "20", "35", "50", "65", "80", "token")
+ehF1 <- melt(ehF1, id = c("sound","vowel", "token"))
+
+# get F2 measures in long format
+ehF2 <- formantData[c(1:2, 8:12, 18)]
+colnames(ehF2) <- c("sound", "vowel", "20", "35", "50", "65", "80", "token")
+ehF2 <- melt(ehF2, id = c("sound","vowel", "token"))
+
+# identify the formant
+ehF1$formant <- "f1"
+ehF2$formant <- "f2"
+
+# store in a single table
+fs <- rbind(ehF1, ehF2)
+fs_head <- subset(fs, vowel == "head")
 
 # plot mean w. 90% confidence interval
-ggplot(ehF1, aes(x = variable, colour = sound)) +
-  stat_summary(aes(y = value, group = sound, fill = sound), fun.data = mean_cl_boot,
-               position = "identity", geom="ribbon", fun.args=list(conf.int=.9), alpha = .5) +  
-  stat_summary(aes(y = value, group = sound), position = "identity", geom="line") +
-  stat_summary(aes(y = value, group = sound, fill = sound),fun.data = mean_cl_boot,
-               position = "identity", geom="ribbon", data = ehF2, fun.args=list(conf.int=.9), alpha = .5) +
-  stat_summary(aes(y = value, group = sound), position = "identity", geom="line", data = ehF2) +
-  labs(x = "Timepoint of measure (as % of vowel)", y = "Formant Freq. in Hz", title = "Head")
+ggplot(fs_head, aes(x = variable, colour = sound)) +
+  stat_summary(aes(y = value, group = interaction(sound, formant), fill = sound),
+               fun.data = mean_cl_boot, fun.args = list(conf.int = .9),
+               geom = "ribbon", alpha = .5) +
+  stat_summary(aes(y = value,  group = interaction(sound, formant)),
+               fun.y = mean, geom = "line") +
+  labs(x = "Timepoint of measure (as % of vowel)",
+       y = "Formant Freq. in Hz",
+       title = "Head")
 
 # same as above with no outline to CI
-ggplot(ehF1, aes(x = variable, colour = sound)) +
-  stat_summary(aes(y = value, group = sound, fill = sound), fun.data = mean_cl_boot,
-               position = "identity", geom="smooth", fun.args=list(conf.int=.9), alpha = .3) +  
-  stat_summary(aes(y = value, group = sound), position = "identity", geom="line") +
-  stat_summary(aes(y = value, group = sound, fill = sound),fun.data = mean_cl_boot,
-               position = "identity", geom="smooth", data = ehF2, fun.args=list(conf.int=.9), alpha = .3) +
-  stat_summary(aes(y = value, group = sound), position = "identity", geom="line", data = ehF2) +
-  labs(x = "Timepoint of measure (as % of vowel)", y = "Formant Freq. in Hz", title = "Head")
+ggplot(fs_head, aes(x = variable, colour = sound)) +
+  stat_summary(aes(y = value, group = interaction(sound, formant),
+                   fill = sound, color = NULL),
+               fun.data = mean_cl_boot, fun.args = list(conf.int = .9),
+               geom = "ribbon", alpha = .5) +
+  stat_summary(aes(y = value,  group = interaction(sound, formant)),
+               fun.y = mean, geom = "line") +
+  labs(x = "Timepoint of measure (as % of vowel)",
+       y = "Formant Freq. in Hz",
+       title = "Head")
+
+# spaghetti plot
+ggplot(fs_head, aes(x = variable, y = value, colour = sound)) +
+  geom_line(aes(group = interaction(token, formant)), alpha = .25) +
+  stat_summary(aes(group = interaction(formant, sound), color = sound),
+               fun.y = mean, geom = "line", size = 1.1) +
+  labs(x = "Timepoint of measure (as % of vowel)",
+       y = "Formant Freq. in Hz",
+       title = "Head")
+
+
+# Why not?
+ggplot(fs, aes(x = variable, y = value, colour = sound)) +
+  geom_line(aes(group = interaction(token, formant)), alpha = .25) +
+  stat_summary(aes(group = interaction(formant, sound), color = sound),
+               fun.y = mean, geom = "line", size = 1.1) +
+  facet_wrap("vowel") +
+  labs(x = "Timepoint of measure (as % of vowel)",
+       y = "Formant Freq. in Hz")
+
